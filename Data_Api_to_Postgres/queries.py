ad625fb8f14fc39b269bbf0d0c553f94245d081a
GGBajaj <girishbajaj2907@gmail.com>
2025-05-04 21:33:22 +0530

    query changed

--- Data_Api_to_Postgres/queries.py
+++ Data_Api_to_Postgres/queries.py
@@ -31,5 +31,6 @@ insert_business_table = """INSERT INTO yelp.business VALUES ('{}', '{}', '{}', '
                         longitude = EXCLUDED.longitude,
                         price = EXCLUDED.price,
                         location = EXCLUDED.location,
-                        phone = EXCLUDED.phone;
+                        phone = EXCLUDED.phone,
+                        email = EXCLUDED.email;
                         """
